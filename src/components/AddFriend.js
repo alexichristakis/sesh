216907ab9a504e1db7aeae8f45021524e5e88e48
Alexi Christakis <alexic@zillowgroup.com>
2018-05-29 14:28:21 -0700

    map view working in Test.js

--- src/components/AddFriend.js
+++ src/components/AddFriend.js
@@ -4,12 +4,14 @@ import { View, Text, Image } from "react-native";
 import { Navigation } from "react-native-navigation";
 
 import BackButton from "./global/BackButton";
+import Test from "./Test";
 
 class AddFriend extends Component {
 	render() {
 		return (
 			<View style={{ flex: 1, padding: 50 }}>
 				<Text>this is the add friend page!</Text>
+				<Test />
 				<BackButton onPressPop={() => Navigation.dismissModal(this.props.componentId)} />
 			</View>
 		);
