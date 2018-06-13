9e8ba7afeca41cdf86ee707b1cc5d32a7f0d9c33
Alexi Christakis <alexic@zillowgroup.com>
2018-06-12 23:22:26 -0700

    new colors

--- src/lib/styles.js
+++ src/lib/styles.js
@@ -1,10 +1,14 @@
+const header1 = "rgba(23,184,255,1)";
+const header2 = "rgba(0,147,255,0.8)";
+
 const primary = "#5AC8FA";
 const secondary = "#FF2C55";
 const tertiary = "#5856d6";
 
 const groups = "#FF9500";
-const active = "#5856d6";
-const later = "#FF2C55";
+// const active = "#5856d6";
+const active = "#9121D3";
+const later = "#FF9500";
 
 /* Apple colors */
 const red = "#FF3B30";
@@ -17,101 +21,107 @@ const purple = "#5856d6";
 const pink = "#FF2C55";
 
 const lightGray = "#F6F6F6";
-const mediumGray = "#EFEFEF";
+// const mediumGray = "#EFEFEF";
+// const darkerGray = "rgb(209,211,212)";
+const mediumGray = "rgb(230,231,232)";
 const gray = "#979797";
 
 const card = {};
 
 const shadow = {
-	shadowRadius: 0.75,
-	shadowOpacity: 0.1,
-	shadowOffset: {
-		height: 0,
-		width: 0,
-	},
-	shadowColor: "#000000",
+  shadowRadius: 0.75,
+  shadowOpacity: 0.1,
+  shadowOffset: {
+    height: 0,
+    width: 0
+  },
+  shadowColor: "#000000"
 };
 
 const iconShadow = {
-	shadowRadius: 20,
-	shadowOpacity: 0.5,
-	shadowOffset: {
-		height: 0,
-		width: 0,
-	},
-	shadowColor: "#000000",
+  shadowRadius: 20,
+  shadowOpacity: 0.5,
+  shadowOffset: {
+    height: 0,
+    width: 0
+  },
+  shadowColor: "#000000"
 };
 
 const lightShadow = {
-	shadowRadius: 5,
-	shadowOpacity: 0.15,
-	shadowOffset: {
-		height: -1,
-		width: 0,
-	},
-	shadowColor: "#000000",
+  shadowRadius: 5,
+  shadowOpacity: 0.15,
+  shadowOffset: {
+    height: -1,
+    width: 0
+  },
+  shadowColor: "#000000"
 };
 
 const shadowBottom = {
-	shadowRadius: 3,
-	shadowOpacity: 0.1,
-	shadowOffset: {
-		height: 4,
-		width: 0,
-	},
-	shadowColor: "#000000",
+  shadowRadius: 3,
+  shadowOpacity: 0.1,
+  shadowOffset: {
+    height: 4,
+    width: 0
+  },
+  shadowColor: "#000000"
 };
 
 const heavyShadow = {
-	shadowRadius: 4,
-	shadowOpacity: 0.1,
-	shadowOffset: {
-		height: 2,
-		width: 0,
-	},
-	shadowColor: "#000000",
+  shadowRadius: 4,
+  shadowOpacity: 0.1,
+  shadowOffset: {
+    height: 2,
+    width: 0
+  },
+  shadowColor: "#000000"
 };
 
 const buttonShadow = {
-	shadowRadius: 3,
-	shadowOpacity: 0.15,
-	shadowOffset: {
-		height: -1,
-		width: 0,
-	},
-	shadowColor: "#000000",
+  shadowRadius: 3,
+  shadowOpacity: 0.15,
+  shadowOffset: {
+    height: -1,
+    width: 0
+  },
+  shadowColor: "#000000"
 };
 
 const cardShadow = {
-	shadowRadius: 30,
-	shadowOpacity: 0.13,
-	shadowOffset: {
-		height: 0,
-		width: 0,
-	},
-	shadowColor: "#000000",
+  shadowRadius: 30,
+  shadowOpacity: 0.13,
+  shadowOffset: {
+    height: 0,
+    width: 0
+  },
+  shadowColor: "#000000"
 };
 
 const colors = {
-	primary,
-	secondary,
-	tertiary,
-	active,
-	later,
-	groups,
-	lightGray,
-	mediumGray,
-	gray,
-	red,
+  header1,
+  header2,
+  primary,
+  secondary,
+  tertiary,
+  active,
+  later,
+  groups,
+  lightGray,
+  mediumGray,
+  // darkerGray,
+  gray,
+  red,
+  green
 };
 
 module.exports = {
-	Colors: colors,
-	shadow: shadow,
-	cardShadow,
-	iconShadow,
-	shadowBottom,
-	lightShadow,
-	heavyShadow,
-	buttonShadow,
+  Colors: colors,
+  shadow: shadow,
+  cardShadow,
+  iconShadow,
+  shadowBottom,
+  lightShadow,
+  heavyShadow,
+  buttonShadow
 };
