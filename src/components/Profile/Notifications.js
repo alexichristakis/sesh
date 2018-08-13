21ab688b660bd682f4ef534f88142a494203e39d
Alexi Christakis <alexi.christakis@gmail.com>
2018-08-12 21:46:23 -0700

    oops needed that lin

--- src/components/Profile/Notifications.js
+++ src/components/Profile/Notifications.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, FlatList, View } from "react-native";
+import { StyleSheet, FlatList, View, Text } from "react-native";
 
 import SuperEllipseMask from "react-native-super-ellipse-mask";
 
